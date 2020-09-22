686cbb790e1331b240e2381a73645d36dcfe32d6
beulasamuel <beaulathamban@gmail.com>
2020-09-23 11:10:45 +1200

    changed visuals

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
   $('.brown').click(function(){
 
     $('body')
-    .css('background','brown')
-
+    // .css('background','brown')
+    .css('background','url("images/brown.jpeg")')
     .append('<h1 id="brown" class="display-1 mt-5 text-light text-center">Parauri</h1>');
 
     $('#everything').hide();
@@ -28,7 +28,8 @@ $(document).ready(function(){
     $('body')
     .css('background','purple')
 
-    .append('<h1 id="purple" class="display-1 mt-5 text-light text-center">Tawa</h1>');
+    .append('<h1 id="purple" class="display-1 mt-5 text-light text-center">Tawa</h1>')
+    .append('<img id="Lavender" src="images/purple.jpeg" alt="Lavender" style="width: 500px; display: block; margin:auto;" >');
 
     $('#everything').hide();
     setTimeout(function(){
@@ -36,7 +37,8 @@ $(document).ready(function(){
     }, 5000);
 
     setTimeout(function(){
-    $('#purple').fadeOut();// or fade, css display however you'd like.
+    $('#purple, #Lavender').fadeOut();// or fade, css display however you'd like.
+
     }, 5000);
 
     $('.purple').text('Tawa');
@@ -46,9 +48,9 @@ $(document).ready(function(){
   $('.orange').click(function(){
 
     $('body')
-    .css('background','orange')
-
-    .append('<h1 id="orange" class="display-1 mt-5 text-light text-center">Karaka</h1>');
+    // .css('background','orange')
+    .css('background','url("images/orange.jpeg")').css('color','orange')
+    .append('<h1 id="orange" class="jumbotron display-1 mt-5 orange text-light text-center">Karaka</h1>');
 
     $('#everything').hide();
     setTimeout(function(){
